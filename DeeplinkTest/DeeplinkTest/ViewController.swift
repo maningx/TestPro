2026127761f1f5420aa23f379279af237f51f242
maningx <ning.ma@smalltreemedia.com>
2019-11-11 15:32:08 +0800

    添加测试项目。

---
+++ DeeplinkTest/DeeplinkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeeplinkTest
+//
+//  Created by maning on 2019/11/11.
+//  Copyright © 2019 maning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
